57c3479f8e890985f0d83469cc8edc5c97068ab5
JIA LIU <timspace1988@hotmail.com>
2017-05-21 15:39:00 +0000

    Create static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>@yield('title', 'Sample') - Laravel project</title>
+    <!-- 'Sample' is the default value of title -->
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
