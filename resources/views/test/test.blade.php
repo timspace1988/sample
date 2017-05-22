497b4052e7ad2c21537f2175a0e1a022e504e3b8
JIA LIU <timspace1988@hotmail.com>
2017-05-22 05:06:17 +0000

    Second test

---
+++ resources/views/test/test.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Another checkout test</title>
+  </head>
+  <body>
+    <h1>Another checkout mand merge test</h1>
+  </body>
+</html>
